7942b472da93c4a90da7a8f3312f1a831f4d9e5b
Don Soltis <don.soltis@joinroot.com>
2022-07-18 08:59:53 -0600

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Donald Soltis on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
