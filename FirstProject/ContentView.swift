3f94ac41cfc1f496970040175e79efdb1dfb3c4f
Don Soltis <don.soltis@joinroot.com>
2022-07-18 09:10:36 -0600

    Test commit

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             } else {
                 Text("")
                     .padding()
+                    .font(.title)
             }
             
             Button(action:  {
